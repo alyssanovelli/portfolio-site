b2cc8aa9ed66821ef0c45142ca5059cd29bcc9ac
alyssanovelli <anovelli028@gmail.com>
2025-10-16 10:44:54 -0400

    Add confirmation

--- darkmode.js
+++ darkmode.js
@@ -12,3 +12,9 @@ if (body.classList.contains("dark-mode")) {
         localStorage.setItem("darkMode", "disabled");
     }
 });
+const form = document.querySelector("form");
+const confirmation = document.getElementById("form-confirmation");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+});
